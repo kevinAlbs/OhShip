a758b888513512e9c91a24b0015201d51dd99e29
Kevin Albertson <ka324@scarletmail.rutgers.edu>
2015-04-10 00:46:52 -0400

    Layout changes

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+(function(){
+  var game = new Phaser.Game(800,600,Phaser.AUTO,document.getElementById("game"), states);
+  var states = {};
+  states.preload = function(){};
+  states.create = function(){};
+  states.update = function(){};
+  states.render = function(){};
+}())
