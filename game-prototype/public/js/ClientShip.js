353f4ddbccfb9373a08a1a803c68aecdb8b0452a
Kevin Albertson <ka324@scarletmail.rutgers.edu>
2016-05-29 23:38:04 -0400

    Networking prototype

--- game-prototype/public/js/ClientShip.js
+++ game-prototype/public/js/ClientShip.js
@@ -14,12 +14,19 @@ var ClientShip = function(createState) {
 	this.tick = function(delta) {
 		refreshStateIfNecessary();
 		updateStateIfNecessary();
+
+		delta /= 30;
+
 		var rotationDelta = (state.leftEngine - state.rightEngine) / 100;
 		var forwardDelta = state.leftEngine + state.rightEngine;
 
 		state.rotation += rotationDelta * delta;
 		state.x += forwardDelta * Math.cos(state.rotation) * delta;
 		state.y += forwardDelta * Math.sin(state.rotation) * delta;
+
+		shipSprite.position.x = state.x;
+		shipSprite.position.y = state.y;
+		shipSprite.rotation = state.rotation;
 	};
 
 	function refreshStateIfNecessary() {
@@ -27,13 +34,17 @@ var ClientShip = function(createState) {
 		// If the network state is far from the current interpolated state
 		// completely replace. Otherwise, inch towards network state.
 		state = refreshState;
+		refreshState = null;
 	}
 
 	function updateStateIfNecessary() {
 		if (!updateState) return;
+		console.log('updating with', updateState);
 		// This should show animations for new events, e.g. if a flag
 		// color is changed, show the flag animation.
 		_.extend(state, updateState);
+		console.log(state);
+		updateState = null;
 	}
 	
 	// Add to rendering.
@@ -42,7 +53,7 @@ var ClientShip = function(createState) {
 		y: 0,
 		leftEngine: 0,
 		rightEngine: 0,
-		rotation: 0
+		rotation: 0,
 		flagColor: 0xFF0000
 	};
 
@@ -52,7 +63,7 @@ var ClientShip = function(createState) {
 
 	_.extend(state, createState);
 
-	var shipTexture = PIXI.Texture.fromImage('ship.png');
+	var shipTexture = PIXI.Texture.fromImage('/img/ship.png');
 	var shipSprite = new PIXI.Sprite(shipTexture);
 	shipSprite.anchor.x = .5;
 	shipSprite.anchor.y = .5;
@@ -60,6 +71,8 @@ var ClientShip = function(createState) {
 	shipSprite.position.x = state.x;
 	shipSprite.position.y = state.y;
 	shipSprite.rotation = state.rotation;
+
+	stage.addChild(shipSprite);
 
 
 };
